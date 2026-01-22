5c9f7115ee74d0ea163991abfb5fcf8c7c461474
Arnau <arnau@ersilia.io>
2026-01-22 11:12:12 +0100

    model final version

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -9,90 +9,76 @@ from rdkit.Chem.Scaffolds import MurckoScaffold
 
 
 def get_canonical_smiles_datamol(smiles):
-    mol = dm.to_mol(smiles)
-    if not mol:
-        return None
-    mol = dm.fix_mol(mol)
-    mol = dm.sanitize_mol(mol)
-    if not mol:
-        return None
-    smi = dm.to_smiles(mol)
-    if not smi:
-        return None
-    m2 = Chem.MolFromSmiles(smi)
-    if not m2:
-        return None
-    return Chem.MolToSmiles(m2, canonical=True, isomericSmiles=True)
+    try:
+        mol = dm.to_mol(smiles)
+        mol = dm.fix_mol(mol)
+        mol = dm.sanitize_mol(mol)
+        smiles = Chem.MolToSmiles(mol, canonical=True, isomericSmiles=True)
+        return mol, smiles
+    except:
+        return None, ""
 
 def get_canonical_smiles_rdkit(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    if not mol:
-        return None
-    return Chem.MolToSmiles(mol, canonical=True, isomericSmiles=True)
+    try:
+        mol = Chem.MolFromSmiles(smiles)
+        smiles = Chem.MolToSmiles(mol, canonical=True, isomericSmiles=True)
+        return mol, smiles
+    except:
+        return None, ""
 
 def get_canonical_smiles(smiles):
     smiles = str(smiles).strip()
     try:
-        canonical_smiles = get_canonical_smiles_datamol(smiles)
-        if canonical_smiles:
-            return canonical_smiles
+        mol, canonical_smiles = get_canonical_smiles_datamol(smiles)
+        return mol, canonical_smiles
     except Exception:
         pass
     try:
-        return get_canonical_smiles_rdkit(smiles)
+        mol, canonical_smiles = get_canonical_smiles_rdkit(smiles)
+        return mol, canonical_smiles
     except Exception:
-        return None
+        return None, ""
     
-def get_standardized_smiles(smiles):
-    if not smiles:
-        return None
-    m = Chem.MolFromSmiles(smiles)
-    if not m:
-        return None
-    m = standardizer.standardize_mol(m)
-    m, _ = standardizer.get_parent_mol(m)
-    if not m:
-        return None
-    m = standardizer.standardize_mol(m)
-    if not m:
-        return None
-    return Chem.MolToSmiles(m, canonical=True, isomericSmiles=True)
-
-def get_flattened_smiles(smiles):
-    if not smiles:
-        return None
-    m = Chem.MolFromSmiles(smiles)
-    if not m:
-        return None
-    return Chem.MolToSmiles(m, canonical=True, isomericSmiles=False)
-         
-def get_murcko_scaffold(smiles):
-    if not smiles:
-        return None
-    m = Chem.MolFromSmiles(smiles)
-    if not m:
-        return None
-    scaf = MurckoScaffold.GetScaffoldForMol(m)
-    if not scaf:
-        return None
-    return Chem.MolToSmiles(scaf, canonical=True)
+def get_standardized_smiles(mol):
+    if mol is None:
+        return None, ""
+    try:
+        mol, _ = standardizer.get_parent_mol(mol)
+        mol = standardizer.standardize_mol(mol)
+        standardized_smiles = Chem.MolToSmiles(mol, canonical=True, isomericSmiles=True)
+        return mol, standardized_smiles
+    except:
+        return None, ""
     
-def get_generic_scaffold(scaf_smiles):
-    if not scaf_smiles:
-        return None
-    scaf = Chem.MolFromSmiles(scaf_smiles)
-    if not scaf:
-        return None
-    generic = MurckoScaffold.MakeScaffoldGeneric(scaf)
-    if not generic:
-        return None
-    return Chem.MolToSmiles(generic, canonical=True)
-
-def deal_with_nones(string):
-    if string is None:
-        return ""
-    return string
-
+def get_flattened_smiles(mol):
+    if mol is None:
+        return None, ""
+    try:
+        flattened_smiles = Chem.MolToSmiles(mol, canonical=True, isomericSmiles=False)
+        mol = Chem.MolFromSmiles(flattened_smiles)
+        return mol, flattened_smiles
+    except:
+        return None, ""
+    
+def get_murcko_scaffold(mol):
+    if mol is None:
+        return None, ""
+    try:
+        mol_scaffold = MurckoScaffold.GetScaffoldForMol(mol)
+        smiles_scaffold = Chem.MolToSmiles(mol_scaffold, canonical=True)
+        return mol_scaffold, smiles_scaffold
+    except:
+        return None, ""
+    
+def get_generic_scaffold(mol_scaffold):
+    if mol_scaffold is None:
+        return None, ""
+    try:
+        mol_generic_scaffold = MurckoScaffold.MakeScaffoldGeneric(mol_scaffold)
+        smiles_generic_scaffold = Chem.MolToSmiles(mol_generic_scaffold, canonical=True)
+        return mol_generic_scaffold, smiles_generic_scaffold
+    except:
+        return None, ""
 
 # parse arguments
 input_file = sys.argv[1]
@@ -100,7 +86,6 @@ output_file = sys.argv[2]
 
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
-
 
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -113,21 +98,14 @@ outputs = []
 
 for smiles in smiles_list:
 
-    canonical_smiles = get_canonical_smiles(smiles)
-    standardized_smiles = get_standardized_smiles(canonical_smiles)
-    flattened_smiles = get_flattened_smiles(standardized_smiles)
-    murcko_scaffold = get_murcko_scaffold(flattened_smiles)
-    generic_scaffold = get_generic_scaffold(murcko_scaffold)
-
-    # Deal with None's
-    canonical_smiles = deal_with_nones(canonical_smiles)
-    standardized_smiles = deal_with_nones(standardized_smiles)
-    flattened_smiles = deal_with_nones(flattened_smiles)
-    murcko_scaffold = deal_with_nones(murcko_scaffold)
-    generic_scaffold = deal_with_nones(generic_scaffold)
+    mol, canonical_smiles = get_canonical_smiles(smiles)
+    mol, standardized_smiles = get_standardized_smiles(mol)
+    mol, flattened_smiles = get_flattened_smiles(mol)
+    mol_scaffold, smiles_scaffold = get_murcko_scaffold(mol)
+    mol_generic_scaffold, smiles_generic_scaffold = get_generic_scaffold(mol_scaffold)
 
     # Store results
-    outputs.append([canonical_smiles, standardized_smiles, flattened_smiles, murcko_scaffold, generic_scaffold])
+    outputs.append([canonical_smiles, standardized_smiles, flattened_smiles, smiles_scaffold, smiles_generic_scaffold])
 
 
 #check input and output have the same lenght
